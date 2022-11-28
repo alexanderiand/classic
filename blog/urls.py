252835593866c76ee0d2df153407bc6b59be8226
David <davidalexanders.prog@gmail.com>
2022-11-29 04:17:53 +0500

    search implementation

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import MainView, CategoryView, PostView, TagView
+from .views import MainView, CategoryView, PostView, TagView, SearchView
 
 urlpatterns = [
 	path('', MainView.as_view(), name='main'),
 	path('category/<str:slug>', CategoryView.as_view(), name='category'),
 	path('view_post/<str:slug>', PostView.as_view(), name='view_post'),
 	path('posts_by_tag/<str:slug>', TagView.as_view(), name="posts_by_tag"),
+	path('search/', SearchView.as_view(), name='search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,19 +6,6 @@ from django.http import Http404
 from .models import Category, Post, Tag
 
 # Create your views here.
-def single(request):
-	return render(request, 'single.html')
-
-def get_category(request, slug):
-	category = Category.objects.all()
-	return render(request, 'category.html', {
-		'category': category,
-	})
-
-def posts_by_tag(request, slug):
-	pass
-
-
 class MainView(ListView):
 	model = Post
 	template_name = 'index.html'
@@ -80,4 +67,17 @@ class TagView(ListView):
 		tag = Tag.objects.get(slug=self.kwargs['slug'])
 		context['title'] = f'Tag: {tag.title}'
 		context['posts'] = Post.objects.filter(tags__slug=self.kwargs['slug'])
+		return context
+
+
+class SearchView(ListView):
+	template_name = 'search.html'
+	context_object_name = 'posts'
+
+	def get_queryset(self):
+		return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['title'] = 'Search'
 		return context
